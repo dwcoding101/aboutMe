9f357411ce15e7d7a39c510b91c531827bea0c62
Daniel <dwcoding101@gmail.com>
2017-12-27 14:28:11 +0000

    build environment set up commit

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,18 @@ var sass        = require('gulp-sass');
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./app"
+        server: "./src"
     });
 
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
+    return gulp.src("src/scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("app/css"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
